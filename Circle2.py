474f29b1a626e5a16f698ca0ffaea9c36bb3da37
Bul4t <enot46161@gmail.com>
2023-09-07 20:14:09 +0300

    Added 4 files

---
+++ Circle2.py
@@ -0,0 +1,8 @@
+num_p = 3.14
+num_radius = input("Введите радиус: ")
+num_length = 2 * num_p * int(num_radius)
+num_area = num_p * int(num_radius) ** 2
+print("Длина окружности: ", num_length)
+print("Площадь окружности: ", num_area)
+print("Нажмите enter!")
+a = input()
